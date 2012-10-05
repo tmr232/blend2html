8303204074c9fd5569d9e712f4e80ddf9d289df2
libra guest <tamir.login.email@gmail.com>
2012-10-05 19:04:43 -0400

    Something is wrong with the flattening - it does not rotate right. probably due to my misunderstanding of blender's rotation model.

--- blend2data.py
+++ blend2data.py
@@ -8,6 +8,10 @@ Y = mathutils.Vector((0, 1, 0))
 Z = mathutils.Vector((0, 0, 1))
 
 def get_rotation(normal):
+    # Make sure the normal is not the Z axis
+    if normal.x == normal.y == 0:
+        return (0, 0, 0)
+    
     # Define local axes
     normal_z = normal
     normal_x = Z.cross(normal_z)
@@ -22,5 +26,30 @@ def get_rotation(normal):
     
     return (0, angle_y, angle_z)
 
+def create_rotation_matrix(x, y, z):
+    def rot_mat(*args): return mathutils.Matrix.Rotation(*args)
+    return rot_mat(z, 4, 'Z') * rot_mat(y, 4, 'Y') * rot_mat(x, 4, 'X')
+
+def flatten_face(face, mesh):
+    # Create the rotation matrix to reverse the rotation
+    rotation = get_rotation(face.normal)
+    print (rotation)
+    rotation_matrix = create_rotation_matrix(*rotation)
+    print (rotation_matrix)
+    inverse_rotation_matrix = rotation_matrix.transposed()
+    
+    flattened_verts = []
+    
+#    for vert in (mesh.vertices[i].co for i in face.vertices):
+#        flattened_verts.append(inverse_rotation_matrix * vert)
+    for i in face.vertices:
+        mesh.vertices[i].co = inverse_rotation_matrix * mesh.vertices[i].co
+        
+    return flattened_verts
+    
+
 normal = bpy.context.selected_objects[0].data.polygons[0].normal
+print(normal)
 print(get_rotation(normal))
+print()
+print(flatten_face(bpy.context.selected_objects[0].data.polygons[0], bpy.context.selected_objects[0].data))
